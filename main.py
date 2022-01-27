bdf7086f560a2e78510c2d753a5f6ecadce92908
D4N1L0-RED <danilodantas7002@gmail.com>
2022-01-27 16:53:51 -0300

    Part 6

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main() -> None:
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Yet Another Roguelike Tutorial",
+        title="Crya's Dungeon",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
